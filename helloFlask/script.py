5b18b9769791310c3b810ac226ee873e72bae467
noemi <noemi.roggero@le-campus-numerique.fr>
2022-08-23 15:29:47 +0200

    Added a first photo

--- helloFlask/script.py
+++ helloFlask/script.py
@@ -8,7 +8,7 @@ def home(name='Noemi'): # association d’une route (URL) avec la fonction ‘ho
     return render_template('index.html', name=name)
 
 @app.route("/cat")    #specifies URL
-def cat(): # This function name is the variable called in 8navigation.html
+def cat(): # This function name is the variable called in _navigation.html
     return render_template('cat.html')
-    
+
 app.run(debug=True) # démarrage de l’application
