490b8c8121b324208242111f8f9fc9db6ced6ef2
noemi <noemi.roggero@le-campus-numerique.fr>
2022-08-23 16:56:29 +0200

    Added more cat photos

--- helloFlask/script.py
+++ helloFlask/script.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__) # instantiation application
+app = Flask(__name__, static_url_path='/static') # instantiation application
 
 @app.route("/")
 def home(name='Noemi'): # association d’une route (URL) avec la fonction ‘home()’
@@ -10,5 +10,13 @@ def home(name='Noemi'): # association d’une route (URL) avec la fonction ‘ho
 @app.route("/cat")    #specifies URL
 def cat(): # This function name is the variable called in _navigation.html
     return render_template('cat.html')
+
+@app.route("/more_cats")    #specifies URL
+def more_cats(): # This function name is the variable called in _navigation.html
+    return render_template('more_cats.html')
+
+@app.route("/AI")    #specifies URL
+def AI(): # This function name is the variable called in _navigation.html
+    return render_template('AI.html')
 
 app.run(debug=True) # démarrage de l’application
