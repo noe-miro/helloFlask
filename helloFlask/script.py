ee49902b3c3bca6205da8219b743c1cad0f08c6f
noemi <noemi.roggero@le-campus-numerique.fr>
2022-08-23 15:04:03 +0200

    Added navigation bar

---
+++ helloFlask/script.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__) # instantiation application
+
+@app.route("/")
+def home(name='Noemi'): # association d’une route (URL) avec la fonction ‘home()’
+    # return "<p>Bienvenue chez moi</p>"  # on renvoie une chaîne de caractères
+    return render_template('index.html', name=name)
+
+@app.route("/cat")    #specifies URL
+def cat(): # This function name is the variable called in 8navigation.html
+    return render_template('cat.html')
+    
+app.run(debug=True) # démarrage de l’application
