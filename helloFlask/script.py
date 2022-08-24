0e436d81c2c394802fa979ad94fc7fbafe8a6c1e
noemi <noemi.roggero@le-campus-numerique.fr>
2022-08-24 13:59:22 +0200

    Added forms

--- helloFlask/script.py
+++ helloFlask/script.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField 
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__, static_url_path='/static') # instantiation application
+app.config["SECRET_KEY"] = str(os.urandom(3).hex()) # for forms
+
+UPLOAD_FOLDER = './helloFlask/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app.config['UPLOAD_PATH'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_PATH'] = 6291456 # max upload size in bytes
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# class MyForm(FlaskForm):
+#     myStringField("My Text")
+#     mySubmitField("Submit My Form")
+messages = [] # for the form
 
 @app.route("/")
 def home(name='Noemi'): # association d’une route (URL) avec la fonction ‘home()’
@@ -19,4 +39,30 @@ def more_cats(): # This function name is the variable called in _navigation.html
 def AI(): # This function name is the variable called in _navigation.html
     return render_template('AI.html')
 
-app.run(debug=True) # démarrage de l’application
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('more_cats'))
+
+    return render_template('create.html')
+
+@app.route('/test_upload')
+def upload_form():
+   return render_template('test_upload.html')
+
+@app.route('/uploader/', methods=('GET', 'POST'))
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_PATH'], secure_filename(f.filename)))
+        return 'file uploaded successfully'
+
+if __name__ == '__main__':
+    app.run(debug=True) # démarrage de l’application
